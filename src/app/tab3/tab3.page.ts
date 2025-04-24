fdb65fcc775ea0aed3be77087f5a6e3c43d1f577
leatile motsosi <151669451+katlegoreloaded@users.noreply.github.com>
2025-04-24 23:45:14 +0200

    set up tab 3 to show completed workout programs

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WorkoutProgram, WorkoutService } from '../services/workout.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -8,6 +10,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  completedWorkouts: WorkoutProgram[] =[];
 
+  constructor(private workoutService: WorkoutService, private navController: NavController) {}
+
+  ionViewWillEnter(){
+    this.loadProgress();
+  }
+
+  async loadProgress(){
+    const ids = await this.workoutService.getCompletedWorkoutIds();
+    const allWorkouts = this.workoutService.getWorkoutPrograms();
+    this.completedWorkouts = allWorkouts.filter(w => ids.includes(w.id));
+  }
+
+  async resetEntireProgress(){
+    await this.workoutService.resetProgress();
+    this.completedWorkouts = [];
+    
+  }
+
+  async resetWorkoutProgress(id: number){
+    await this.workoutService.resetWorkoutProgress(id);
+    this.loadProgress();
+  }
+
+  backHome(){
+    this.navController.navigateForward('/tabs/tab1');
+  }
+  
 }
